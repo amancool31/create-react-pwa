e27d0fbeecb7d396e51e3e70dad8442b6092bbd0
amancool31 <amandeepsinghmadan@yahoo.com>
2018-11-18 03:21:27 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import a from './a.jpg';
+import { Button, Segment } from 'semantic-ui-react';
+import { Link ,Switch,BrowserRouter,Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Segment inverted>
+      <Link to="/map"><Button inverted color='teal'>
+        Map
+      </Button></Link>
+      <Link to="/register">
+      <Button inverted color='teal'>
+        Register 
+      </Button></Link>
+      <Link to="/feedback">
+      <Button inverted color='teal'>
+        FeedBack
+      </Button>
+      </Link>
+      <Link to="/contact">
+      <Button inverted color='teal'>
+        Contact us
+      </Button>
+      </Link>
+      </Segment>
+        <Particles params={{
+            		particles: {
+                  interactivity:{
+                    detect_on:"canvas",
+                    events:
+                    {onhover:{enable:true,mode:"repulse"},onclick:{enable:true,mode:"push"},resize:true},modes:{grab:{distance:400}}},
+            			line_linked: {
+            				shadow: {
+            					enable: true,
+            					color: "#3CA9D1",
+            					//blur: 3
+            				}
+            			}
+            		}
+            	}}
+              style={{
+                width: '100%',
+                //backgroundImage: `url(${a})` ,
+                background: "#6778c8",
+              }}/>
+              <BrowserRouter>
+              <Route path='/register'></Route>
+              <Route path='/contact'></Route>
+              <Route path='/feedback'></Route>
+              <Route path='/map'></Route>
+              </BrowserRouter>
+
       </div>
     );
   }
